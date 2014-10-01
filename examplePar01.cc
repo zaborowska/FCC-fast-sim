4284c194241d3058a9577563045669cb4af01dde
zaborowska <anna.zaborowska@cern.ch>
2014-10-01 18:24:02 +0200

    Geometry read from gdml - sample file from G04 example

--- examplePar01.cc
+++ examplePar01.cc
@@ -49,8 +49,11 @@
 //------------
 // Geometries:
 //------------
-#include "Par01DetectorConstruction.hh"
 #include "Par01ParallelWorldForPion.hh"
+#include "G01DetectorConstruction.hh"
+#include "G4GDMLParser.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4TransportationManager.hh"
 
 //-----------------------------------
 // Par01PhysicsList (makes use of the
@@ -74,8 +77,14 @@
 #include "G4UIExecutive.hh"
 #endif
 
+
 int main(int argc, char** argv)
 {
+
+   G4GDMLParser parser;
+  // Load geometry
+  G4cout << "Geometry loaded from  file ....... FCCdetector.gdml" << G4endl;
+   parser.Read("FCCdetector.gdml");
   //-------------------------------
   // Initialization of Run manager
   //-------------------------------
@@ -93,7 +102,7 @@ int main(int argc, char** argv)
 #endif
 
   // Detector/mass geometry and parallel geometry(ies):
-  G4VUserDetectorConstruction* detector = new Par01DetectorConstruction();
+  G4VUserDetectorConstruction* detector = new G01DetectorConstruction(parser.GetWorldVolume());
   // -- Parallel geometry for pion parameterisation
   detector->RegisterParallelWorld(new Par01ParallelWorldForPion("pionGhostWorld"));
   // --  The name passed must be the same passed to the
@@ -112,14 +121,35 @@ int main(int argc, char** argv)
   // Initialize Run manager
   runManager->Initialize();
 
-  //----------------
-  // Visualization:
-  //----------------
-#ifdef G4VIS_USE
-  G4cout << "Instantiating Visualization Manager......." << G4endl;
-  G4VisManager* visManager = new G4VisExecutive;
-  visManager -> Initialize ();
-#endif
+
+
+   ///////////////////////////////////////////////////////////////////////
+   //
+   // Example how to retrieve Auxiliary Information
+   //
+   const G4LogicalVolumeStore* lvs = G4LogicalVolumeStore::GetInstance();
+   std::vector<G4LogicalVolume*>::const_iterator lvciter;
+   for( lvciter = lvs->begin(); lvciter != lvs->end(); lvciter++ )
+   {
+     G4GDMLAuxListType auxInfo = parser.GetVolumeAuxiliaryInformation(*lvciter);
+     std::vector<G4GDMLAuxPairType>::const_iterator ipair = auxInfo.begin();
+     for( ipair = auxInfo.begin(); ipair != auxInfo.end(); ipair++ )
+     {
+       G4String str=ipair->type;
+       G4String val=ipair->value;
+       G4cout << " Auxiliary Information is found for Logical Volume :  "
+              << (*lvciter)->GetName() << G4endl;
+       G4cout << " Name of Auxiliary type is     :  " << str << G4endl;
+       G4cout << " Associated Auxiliary value is :  " << val << G4endl;
+     }
+   }
+   //
+   // End of Auxiliary Information block
+   //
+   ////////////////////////////////////////////////////////////////////////
+
+
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
   if(argc==1)
   {
@@ -128,7 +158,15 @@ int main(int argc, char** argv)
     //--------------------------
 #ifdef G4UI_USE
     G4UIExecutive * ui = new G4UIExecutive(argc, argv);
+#ifdef G4VIS_USE
+     G4VisManager* visManager = new G4VisExecutive;
+     visManager->Initialize();
+     UImanager->ApplyCommand("/control/execute vis.mac");
+#endif
     ui->SessionStart();
+#ifdef G4VIS_USE
+  delete visManager;
+#endif
     delete ui;
 #endif
   }
@@ -136,7 +174,6 @@ int main(int argc, char** argv)
   {
     G4String command = "/control/execute ";
     G4String fileName = argv[1];
-    G4UImanager * UImanager = G4UImanager::GetUIpointer();
     UImanager->ApplyCommand(command+fileName);
   }
 
@@ -144,9 +181,6 @@ int main(int argc, char** argv)
   //                 owned and deleted by the run manager, so they should not
   //                 be deleted in the main() program !
 
-#ifdef G4VIS_USE
-  delete visManager;
-#endif
   delete runManager;
 
   return 0;
