442486aaba6b7c0cbba0ef7f887cf0e9a7dc8f9c
thwillia <themistoklis.williams@cern.ch>
2014-10-07 18:30:16 +0200

    Example with Sensitive Detector

--- include/B2TrackerSD.hh
+++ include/B2TrackerSD.hh
@@ -23,23 +23,47 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
+// $Id: B2TrackerSD.hh 69706 2013-05-13 09:12:40Z gcosmo $
 //
-// $Id: Par01ParallelWorldForPion.hh 77659 2013-11-27 08:57:46Z gcosmo $
-//
-#ifndef Par01ParallelWorldForPion_hh
-#define Par01ParallelWorldForPion_hh
+/// \file B2TrackerSD.hh
+/// \brief Definition of the B2TrackerSD class
 
-#include "G4VUserParallelWorld.hh"
+#ifndef B2TrackerSD_h
+#define B2TrackerSD_h 1
 
-class Par01ParallelWorldForPion : public G4VUserParallelWorld {
-public:
-  Par01ParallelWorldForPion(G4String worldName);
-  ~Par01ParallelWorldForPion();
+#include "G4VSensitiveDetector.hh"
+
+#include "B2TrackerHit.hh"
+
+#include <vector>
+
+class G4Step;
+class G4HCofThisEvent;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+/// B2Tracker sensitive detector class
+///
+/// The hits are accounted in hits in ProcessHits() function which is called
+/// by Geant4 kernel at each step. A hit is created with each step with non zero 
+/// energy deposit.
+
+class B2TrackerSD : public G4VSensitiveDetector
+{
+  public:
+    B2TrackerSD(const G4String& name, 
+                const G4String& hitsCollectionName);
+    virtual ~B2TrackerSD();
   
-private:
-  virtual void Construct();
-  virtual void ConstructSD();
+    // methods from base class
+    virtual void   Initialize(G4HCofThisEvent* hitCollection);
+    virtual G4bool ProcessHits(G4Step* step, G4TouchableHistory* history);
+    virtual void   EndOfEvent(G4HCofThisEvent* hitCollection);
 
+  private:
+    B2TrackerHitsCollection* fHitsCollection;
 };
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #endif
